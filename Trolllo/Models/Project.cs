3022def098b792f10838c1b19f7ef14d7f170891
MatSik <Mateusz.Sikora.95@wp.pl>
2016-11-10 22:58:06 +0100

    first commit

---
+++ Trolllo/Models/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trolllo.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public IList<WorkTask> WorkTasks { get; set; }
+
+        public int Id { get; set; }
+    }
+}
---
+++ Trolllo/Models/WorkTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trolllo.Models
+{
+    public class WorkTask
+    {
+        public int WorkTaskId { get; set; }
+
+        public string Task { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public State State { get; set; }
+
+        public int Difficulty { get; set; }
+    }
+
+    public enum State
+    {
+        NotAssigned,
+        InProgress,
+        Canceled,
+        Completed,
+        Failed
+    }
+}
